349e69b8dfc34d8ffe5bc0cfa9db49dd7aebf313
bishaldsrija08 <bishaldsrijal1@gmail.com>
2025-01-20 23:51:28 +0545

    Project Setup, Redux Toolkit

---
+++ store/authSlice.js
@@ -0,0 +1,25 @@
+import {createSlice} from '@reduxjs/toolkit'
+
+const authSlice = createSlice({
+    name: 'auth',
+    initialState: {
+        user: null,
+        token: null,
+        status: null
+    },
+    reducers: {
+        setStatus(state, action){
+            state.status = action.payload
+        },
+        setUser(state, action){
+            state.user = action.payload
+        },
+        setToken(state, action){
+            state.token = action.payload
+        }
+    }
+})
+
+export const {setStatus, setUser, setToken} = authSlice.actions
+
+export default authSlice.reducer
